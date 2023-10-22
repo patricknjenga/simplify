c17320bedfdd8def407ff67edc78898a49ae9591
patrick <patrick.njenga@outlook.com>
2023-10-22 14:19:40 +0300

    ++

--- errors/main.go
+++ errors/main.go
@@ -8,7 +8,7 @@ import (
 
 func Format(err error) error {
 	_, file, line, _ := runtime.Caller(1)
-	return fmt.Errorf("%s : %d : %w", file, line, err)
+	return fmt.Errorf("%s:%d %w", file, line, err)
 }
 
 func Log(err error) {
