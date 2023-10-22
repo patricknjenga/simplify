7a57ba902affacfef34387ff2d90f90a55ca7408
patrick <patrick.njenga@outlook.com>
2023-10-22 14:19:28 +0300

    ++

--- errors/main.go
+++ errors/main.go
@@ -1,34 +1,16 @@
 package errors
 
 import (
-	"encoding/json"
-	"os"
+	"fmt"
+	"log"
 	"runtime"
-	"time"
 )
 
-func ArrDo(functions []func() error) error {
-	for _, function := range functions {
-		err := function()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+func Format(err error) error {
+	_, file, line, _ := runtime.Caller(1)
+	return fmt.Errorf("%s : %d : %w", file, line, err)
 }
 
 func Log(err error) {
-	_, file, line, _ := runtime.Caller(1)
-	var e = struct {
-		File    string
-		Line    int
-		Message string
-		Time    string
-	}{
-		File:    file,
-		Line:    line,
-		Message: err.Error(),
-		Time:    time.Now().Format("20060102150405"),
-	}
-	json.NewEncoder(os.Stderr).Encode(&e)
+	log.Println(err)
 }
