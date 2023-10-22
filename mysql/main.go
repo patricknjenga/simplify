67b2a75ddf1ce8c94ab493819ff85ea9c5c9ab51
patrick <patrick.njenga@outlook.com>
2023-10-22 16:48:47 +0300

    ++

--- mysql/main.go
+++ mysql/main.go
@@ -7,18 +7,18 @@ import (
 	"gorm.io/gorm"
 )
 
-func New(dsn string, name string, models ...any) error {
+func New(dsn string, name string, models ...any) (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return err
+		return nil, err
 	}
 	err = db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", name)).Error
 	if err != nil {
-		return err
+		return nil, err
 	}
 	err = db.Exec(fmt.Sprintf("USE %s", name)).Error
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return db.AutoMigrate(models...)
+	return db, db.AutoMigrate(models...)
 }
