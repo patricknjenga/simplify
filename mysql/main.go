802a521d95bc67a6c0300c0797beae50e90ce7b1
patrick <patrick.njenga@outlook.com>
2023-11-02 13:42:47 +0300

    ++

--- mysql/main.go
+++ mysql/main.go
@@ -7,16 +7,16 @@ import (
 	"gorm.io/gorm"
 )
 
-func New(dsn string, name string, models ...any) (*gorm.DB, error) {
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func New(user string, pass string, host string, port string, database string, models ...any) (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/sys?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port)), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-	err = db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", name)).Error
+	err = db.Exec(fmt.Sprintf("create database if not exists %s", database)).Error
 	if err != nil {
 		return nil, err
 	}
-	err = db.Exec(fmt.Sprintf("USE %s", name)).Error
+	db, err = gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, database)), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
